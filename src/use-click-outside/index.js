8759bdfb37491fad672ad99973aaf4b03a6df503
Matt Milburn <me@mattmilburn.com>
2021-08-25 09:40:46 -0500

    Simplify useClickOutside logic

--- src/use-click-outside/index.js
+++ src/use-click-outside/index.js
@@ -1,42 +1,21 @@
 import { useEffect } from 'react';
 
-export const findNearestAncestor = ( query, node ) => {
-  // Stop searching if we ran out of ancestors.
-  if ( ! node || node === document ) {
-    return undefined;
-  }
-
-  // Match against the DOM node.
-  if ( node === query ) {
-    return node;
-  }
-
-  // Match against a string selector.
-  if ( typeof query === 'string' ) {
-    // Remove potential leading # or . characters from the query.
-    const queryValue = query.slice( 1 );
-
-    // Match against tag name.
-    if ( node.tagName.toLowerCase() === query ) {
-      return node;
-    }
-
-    // Match against ID attribute.
-    if ( query.charAt( 0 ) === '#' && node.id === queryValue ) {
-      return node;
-    }
-
-    // Match against class name.
-    if ( node.classList.contains( queryValue ) ) {
-      return node;
-    }
-  }
-
-  // Run recursively until we find a match or run out of nodes to compare against.
-  return findNearestAncestor( query, node.parentNode );
-}
-
 const useClickOutside = ( ref, onClickOutside ) => {
+  const inBounds = node => {
+    // Return false if we ran out of ancestors.
+    if ( ! node || node === document ) {
+      return false;
+    }
+
+    // Return true if we found boundary.
+    if ( node === ref.current ) {
+      return true;
+    }
+
+    // Run recursively until we reach a boundary or run out of nodes to compare against.
+    return inBounds( node.parentNode );
+  };
+
   useEffect( () => {
     const onClick = event => {
       // Do nothing if we don't have a ref (yet).
@@ -44,8 +23,8 @@ const useClickOutside = ( ref, onClickOutside ) => {
         return;
       }
 
-      // If user click target exists outside of `ref.current`, run the callback.
-      if ( ! findNearestAncestor( ref.current, event.target ) ) {
+      // If the click target exists outside of `ref.current`, run the callback.
+      if ( ! inBounds( event.target ) ) {
         onClickOutside();
       }
     };
