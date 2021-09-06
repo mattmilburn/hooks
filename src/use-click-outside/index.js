df04a4b6b15d5c20ed65d68b35aed47ef1ed035b
Matt Milburn <me@mattmilburn.com>
2021-09-06 15:34:35 -0500

    Greatly simplify useClickOutside logic

--- src/use-click-outside/index.js
+++ src/use-click-outside/index.js
@@ -1,30 +1,14 @@
 import { useEffect } from 'react';
 
 const useClickOutside = ( ref, onClickOutside ) => {
-  const inBounds = node => {
-    // Return false if we ran out of ancestors.
-    if ( ! node || node === document ) {
-      return false;
-    }
-
-    // Return true if we found boundary.
-    if ( node === ref.current ) {
-      return true;
-    }
-
-    // Run recursively until we reach a boundary or run out of nodes to compare against.
-    return inBounds( node.parentNode );
-  };
-
   useEffect( () => {
     const onClick = event => {
-      // Do nothing if we don't have a ref (yet).
       if ( ! ref.current ) {
         return;
       }
 
-      // If the click target exists outside of `ref.current`, run the callback.
-      if ( ! inBounds( event.target ) ) {
+      // If `ref.current` does not contain `event.target` as a descendant, run the callback.
+      if ( ! ref.current.contains( event.target ) ) {
         onClickOutside();
       }
     };
