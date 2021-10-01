f987db406f71404993cf5d785d7d6fe8f31c44a0
Matt Milburn <me@mattmilburn.com>
2021-10-01 18:34:15 -0500

    Refactor useLocalStorage hook to keep components in sync

--- src/use-local-storage/index.js
+++ src/use-local-storage/index.js
@@ -2,55 +2,54 @@ import { useEffect, useState } from 'react';
 
 const ONE_DAY = 1000 * 60 * 60 * 24;
 
-const useLocalStorage = key => {
-  const [ value, setValue ] = useState( null );
+const useLocalStorage = ( key, initialValue = null ) => {
+  const [ value, setValue ] = useState( initialValue );
+
+  let storedValue = null;
+
+  // Try to get value from localStorage.
+  try {
+    storedValue = typeof window !== 'undefined' && window.localStorage.getItem( key );
+  } catch( err ) {
+    console.log( err );
+  }
 
   const setStoredValue = ( val, duration = ONE_DAY ) => {
+    const data = JSON.stringify( {
+      expDate: Date.now() + duration,
+      data: val,
+    } );
+
+    // Save to localStorage.
     try {
-      const data = JSON.stringify( {
-        expDate: Date.now() + duration,
-        data: val,
-      } );
-
-      // Save to localStorage.
-      const storedValue = typeof window !== 'undefined' && window.localStorage.setItem( key, data );
-
-      if ( ! storedValue ) {
-        return;
-      }
-
-      setValue( val );
+      storedValue = typeof window !== 'undefined' && window.localStorage.setItem( key, data );
     } catch( err ) {
-      // Do nothing if localStorage is not supported.
       console.log( err );
     }
   };
 
   useEffect( () => {
-    try {
-      // Get initial value from localStorage, if it exists.
-      const storedValue = typeof window !== 'undefined' && window.localStorage.getItem( key );
-
-      if ( ! storedValue ) {
-        return;
-      }
-
-      const { expDate, data } = JSON.parse( storedValue );
-
-      // Remove data if its expired.
-      if ( Date.now() >= expDate ) {
-        window.localStorage.removeItem( key );
-        setValue( null );
-        return;
-      }
-
-      // Return stored data.
-      setValue( data );
-    } catch( err ) {
-      // Do nothing if localStorage is not supported.
-      console.log( err );
+    if ( ! storedValue ) {
+      return;
     }
-  }, [] );
+
+    const { expDate, data } = JSON.parse( storedValue );
+
+    // Remove data if it expired.
+    if ( Date.now() >= expDate ) {
+      try {
+        window.localStorage.removeItem( key );
+      } catch( err ) {
+        console.log( err );
+      }
+
+      setValue( null );
+      return;
+    }
+
+    // Return stored data.
+    setValue( data );
+  }, [ storedValue ] );
 
   return [
     value,
